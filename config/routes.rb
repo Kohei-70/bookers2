091d375a6d4fccb4a85bd8ec5842c2a08affb818
Kohei-70 <152633774+Kohei-70@users.noreply.github.com>
2024-01-28 05:38:32 +0000

    [completed bookers2]

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  root to: "homes#top"
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'home/about' => 'homes#about'
+  post 'books' => 'books#index' 
+
+
+end
